2c71cdd9bd0bb79d52c60e36240aa68ac40b3f6d
Tilak Sharma <tilak.sharma.mail@gmail.com>
2019-07-14 17:37:55 +0530

    added rank component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import Rank from './components/rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm';
+import Particles from 'react-particles-js';
 // import FaceRecognition from './components/Logo';
 
 import './App.css';
 
+const particleOptions = {
+  "particles": {
+    "number": {
+        "value": 200
+    },
+    "size": {
+        "value": 3
+    }
+},
+"color": {
+  "value": "#E86363"
+}
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles 
+        params={particleOptions} 
+        className="particles"
+      />
       <Navigation/>
       <Logo/>
+      <Rank/>
       <ImageLinkForm/>
       {/* 
       
